d99fb930cd5961095698a38a0605a218b1885d28
ryanmarques <47296650+ryanmarques1@users.noreply.github.com>
2024-06-04 18:50:46 -0300

    Versão Final

--- ScriptBanco.sql
+++ ScriptBanco.sql
@@ -1,9 +1,10 @@
 --Execute no phpMyAdmin
 
 CREATE DATABASE cadastro_nis;
-
+USE cadastro_nis;
 CREATE TABLE usuarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     NIS VARCHAR(11) NOT NULL
 );
+
